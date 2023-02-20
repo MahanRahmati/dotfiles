2b74288feb5e7815facd275c759a4dc8db1096e2
MahanRahmati <mahan.rahmati@protonmail.com>
2023-02-21 00:41:47 +0330

    [Neovim] Update Telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -10,6 +10,8 @@ telescope.setup {
   defaults = {
     path_display = { "smart" },
     layout_strategy = "flex",
+    prompt_prefix = " ",
+    selection_caret = " ",
     file_ignore_patterns = {
       ".git/",
       "%.lock",
