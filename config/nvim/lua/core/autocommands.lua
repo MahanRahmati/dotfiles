af7616543d6a8b4c6bff8ed0f9f7b4ce8e1ab3ad
MahanRahmati <mahan.rahmati@protonmail.com>
2023-03-24 23:13:16 +0330

    [Neovim] Update autocommands

--- config/nvim/lua/core/autocommands.lua
+++ config/nvim/lua/core/autocommands.lua
@@ -55,7 +55,9 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 vim.api.nvim_create_autocmd("BufReadPost", {
   pattern = "*",
   callback = function()
-    vim.cmd [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g'\"" | endif]]
+    if vim.fn.line "'\"" > 0 and vim.fn.line "'\"" <= vim.fn.line "$" then
+      vim.fn.setpos(".", vim.fn.getpos "'\"")
+    end
   end,
 })
 
