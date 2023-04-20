698dd6fb0046fd69581b3126ea6e14c49e2c9a6b
MahanRahmati <mahan.rahmati@protonmail.com>
2023-04-20 18:46:10 +0330

    [Neovim] Remove warnings

--- config/nvim/ftplugin/rust.lua
+++ config/nvim/ftplugin/rust.lua
@@ -1,6 +1,6 @@
 local status_ok, which_key = pcall(require, "which-key")
 if not status_ok then
-  vim.notify("Failed to load which-key", "error")
+  vim.notify("Failed to load which-key", vim.log.levels.ERROR)
   return
 end
 
