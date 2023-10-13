8903c5b84b80e9cf61cdfc0575673aa58c4b0014
MahanRahmati <mahan.rahmati@protonmail.com>
2023-10-13 13:30:58 +0330

    [Neovim] Update center cursor autocmd

--- config/nvim/lua/core/center-cursor.lua
+++ config/nvim/lua/core/center-cursor.lua
@@ -1,6 +1,5 @@
 vim.api.nvim_create_autocmd({
   "CursorMoved",
-  "CursorMovedI",
   "BufEnter",
   "TextChanged",
   "WinScrolled",
@@ -16,5 +15,23 @@ vim.api.nvim_create_autocmd({
       return
     end
     vim.cmd "norm! zz"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "CursorMovedI" }, {
+  group = vim.api.nvim_create_augroup("CenterCursor", { clear = true }),
+  pattern = { "*" },
+  callback = function()
+    if
+        vim.bo.filetype == "alpha"
+        or vim.bo.filetype == "TelescopePrompt"
+        or vim.bo.filetype == "TelescopeResults"
+    then
+      return
+    end
+    local line = vim.api.nvim_win_get_cursor(0)[1]
+    vim.cmd "norm! zz"
+    local column = vim.fn.getcurpos()[5]
+    vim.fn.cursor { line, column }
   end,
 })
