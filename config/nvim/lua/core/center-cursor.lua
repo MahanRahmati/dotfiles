95abba68842e38922478acd5e4c75eb4ce39b863
MahanRahmati <mahan.rahmati@protonmail.com>
2023-10-13 13:14:38 +0330

    [Neovim] Update center cursor autocmd

--- config/nvim/lua/core/center-cursor.lua
+++ config/nvim/lua/core/center-cursor.lua
@@ -8,7 +8,11 @@ vim.api.nvim_create_autocmd({
   group = vim.api.nvim_create_augroup("CenterCursor", { clear = true }),
   pattern = { "*" },
   callback = function()
-    if vim.bo.filetype == "alpha" then
+    if
+        vim.bo.filetype == "alpha"
+        or vim.bo.filetype == "TelescopePrompt"
+        or vim.bo.filetype == "TelescopeResults"
+    then
       return
     end
     vim.cmd "norm! zz"
