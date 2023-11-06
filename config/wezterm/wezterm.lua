2c0623ebb57fe036cf9f769f84d8221b86c6a0f6
MahanRahmati <mahan.rahmati@protonmail.com>
2023-11-06 12:59:58 +0330

    [WezTerm] Update options

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,17 +7,21 @@ return {
   colors = {
     background = "#0E1116",
   },
+  enable_tab_bar = false,
   font = wezterm.font "Hack Nerd Font",
   font_size = 16,
-  hide_tab_bar_if_only_one_tab = true,
-  -- macos_window_background_blur = 20,
-  show_tab_index_in_tab_bar = false,
-  -- window_background_opacity = 0.90,
-  -- window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+  keys = {
+    {
+      key = "t",
+      mods = "CMD",
+      action = wezterm.action.SplitHorizontal,
+    },
+  },
+  window_decorations = "INTEGRATED_BUTTONS|RESIZE",
   window_padding = {
-    left = "0.25cell",
-    right = "0.25cell",
-    top = "0.25cell",
-    bottom = "0.25cell",
+    left = "0.5cell",
+    right = "0.5cell",
+    top = "1.5cell",
+    bottom = "0.0cell",
   },
 }
