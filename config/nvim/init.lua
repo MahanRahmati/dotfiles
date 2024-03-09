0d627fb38ddf5e51724507e504811cbcf96617fa
Mahan <mahan.rahmati@protonmail.com>
2024-03-09 14:41:47 -0500

    Add plugins and alpha

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -8,26 +8,4 @@
 require "options"
 require "autocommands"
 require "keymaps"
-require "core.plugins"
-
-----------------------------------------------------------------------
---                       Load Configurations                        --
-----------------------------------------------------------------------
-
-for _, source in ipairs {
-  "core.center-cursor",
-  "core.usercommands",
-  "plugins.notify",
-  "core.luasnip",
-  "core.cmp",
-  "core.lsp",
-  "plugins.flutter-tools",
-  "plugins.mini",
-  "plugins.sg",
-  "plugins.telescope",
-} do
-  local status_ok, error = pcall(require, source)
-  if not status_ok then
-    vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. error)
-  end
-end
+require "plugins"
