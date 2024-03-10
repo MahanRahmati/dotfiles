e80ab803c9d18eb21b2ede2dd2c2dbddfa611352
Mahan <mahan.rahmati@protonmail.com>
2024-03-10 19:00:29 -0400

    Revert "[WezTerm] Add logic for auto appearance"
    This reverts commit 57f9a8a1ae19bec548c98ebc24264ec203b3faa5.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,38 +1,12 @@
 local wezterm = require "wezterm"
 
-local function scheme_for_appearance(appearance)
-  if appearance:find "Dark" then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
-  end
-end
-
-local function background_color(appearance)
-  if appearance:find "Dark" then
-    return "#0E1116"
-  else
-    return "#EFF1F5"
-  end
-end
-
-wezterm.on("window-config-reloaded", function(window, _)
-  local overrides = window:get_config_overrides() or {}
-  local appearance = window:get_appearance()
-  local scheme = scheme_for_appearance(appearance)
-  local background = background_color(appearance)
-  if overrides.color_scheme ~= scheme then
-    overrides.color_scheme = scheme
-    overrides.colors = {
-      background = background,
-    }
-    window:set_config_overrides(overrides)
-  end
-end)
-
 return {
   bidi_enabled = true,
   check_for_updates = false,
+  color_scheme = "Catppuccin Mocha",
+  colors = {
+    background = "#0E1116",
+  },
   enable_tab_bar = false,
   font = wezterm.font("Hack Nerd Font", { weight = "Regular" }),
   font_size = 16,
