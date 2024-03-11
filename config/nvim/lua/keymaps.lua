5a620e46fff1cc15965a85c9c7ce6f92a8932276
Mahan <mahan.rahmati@protonmail.com>
2024-03-11 16:33:28 -0400

    Update keymaps.lua

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -25,3 +25,11 @@ set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Replace current word
+set(
+  "n",
+  "<leader>s",
+  ":%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>",
+  { desc = "Replace current word" }
+)
