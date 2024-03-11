cc555a5040dc33b04a5540b2fc6e349a01347cea
Mahan <mahan.rahmati@protonmail.com>
2024-03-11 16:33:34 -0400

    Update sg.lua

--- config/nvim/lua/plugins/sg.lua
+++ config/nvim/lua/plugins/sg.lua
@@ -4,5 +4,42 @@ return {
     opts = {
       enable_cody = true,
     },
+    -- keys = {
+    --   { "<leader>cc", "<cmd>CodyToggle<CR>", desc = "Cody Toggle" },
+    --   {
+    --     "<leader>ch",
+    --     "<cmd>lua require('sg.cody.commands').focus_history()<CR>",
+    --     desc = "Cody Focus History",
+    --   },
+    --   {
+    --     "<leader>cp",
+    --     "<cmd>lua require('sg.cody.commands').focus_prompt()<CR>",
+    --     desc = "Cody Focus Prompt",
+    --   },
+    --   {
+    --     "<leader>cd",
+    --     "<cmd>CodyAsk Generate code documentation<CR>",
+    --     mode = "v",
+    --     desc = "Cody Documentation",
+    --   },
+    --   {
+    --     "<leader>ce",
+    --     "<cmd>CodyAsk Explain code<CR>",
+    --     mode = "v",
+    --     desc = "Cody Explain",
+    --   },
+    --   {
+    --     "<leader>cu",
+    --     "<cmd>CodyAsk Generate unit tests<CR>",
+    --     mode = "v",
+    --     desc = "Cody Unit Test",
+    --   },
+    --   {
+    --     "<leader>cs",
+    --     "<cmd>CodyAsk Find code smells<CR>",
+    --     mode = "v",
+    --     desc = "Cody Smell",
+    --   },
+    -- },
   },
 }
