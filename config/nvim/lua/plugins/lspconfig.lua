6f4c6502780e94053b42396b98fc8f3f8f82eb14
Mahan <mahan.rahmati@protonmail.com>
2024-03-15 18:26:09 -0400

    Update lspconfig.lua

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -120,14 +120,6 @@ return {
         lua_ls = {
           settings = {
             Lua = {
-              runtime = { version = "LuaJIT" },
-              workspace = {
-                checkThirdParty = false,
-                library = {
-                  "${3rd}/luv/library",
-                  unpack(vim.api.nvim_get_runtime_file("", true)),
-                },
-              },
               completion = {
                 callSnippet = "Replace",
               },
