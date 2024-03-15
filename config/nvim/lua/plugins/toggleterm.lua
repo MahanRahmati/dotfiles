820e41c529f48be8820126ce3be6bc4a0d1f195d
Mahan <mahan.rahmati@protonmail.com>
2024-03-15 17:28:36 -0400

    Update toggleterm.lua

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -2,38 +2,39 @@ return {
   {
     "akinsho/toggleterm.nvim",
     event = "VeryLazy",
-    config = function()
-      require("toggleterm").setup {
-        size = 15,
-        hide_numbers = true,
-        shade_filetypes = {},
-        shade_terminals = true,
-        shading_factor = 2,
-        start_in_insert = true,
-        insert_mappings = true,
-        terminal_mappings = true,
-        persist_size = true,
-        direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float',
-        close_on_exit = true,
-        shell = vim.o.shell,
-        float_opts = {
-          border = "curved",
-          winblend = 0,
-          highlights = {
-            border = "Normal",
-            background = "Normal",
-          },
-          width = function(_term)
-            _term.float_opts.col = vim.o.columns
-            return vim.o.columns
-          end,
-          height = function(_term)
-            local height = 15
-            _term.float_opts.row = vim.o.lines - height - 3
-            return height
-          end,
+    opts = {
+      size = 15,
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      persist_size = true,
+      direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float',
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = "rounded",
+        winblend = 0,
+        highlights = {
+          border = "Normal",
+          background = "Normal",
         },
-      }
+        width = function(_term)
+          _term.float_opts.col = vim.o.columns
+          return vim.o.columns
+        end,
+        height = function(_term)
+          local height = 15
+          _term.float_opts.row = vim.o.lines - height - 3
+          return height
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
 
       local Terminal = require("toggleterm.terminal").Terminal
 
@@ -52,18 +53,10 @@ return {
       function _LAZYGIT_TOGGLE()
         lazygit:toggle()
       end
-      vim.keymap.set(
-        "n",
-        "<leader>gg",
-        "<cmd>lua _LAZYGIT_TOGGLE()<CR>",
-        { desc = "LazyGit" }
-      )
-      vim.keymap.set(
-        "n",
-        "<leader>t",
-        "<cmd>ToggleTerm<CR>",
-        { desc = "Terminal" }
-      )
     end,
+    keys = {
+      { "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", desc = "LazyGit" },
+      { "<leader>t", "<cmd>ToggleTerm<CR>", desc = "Terminal" },
+    },
   },
 }
