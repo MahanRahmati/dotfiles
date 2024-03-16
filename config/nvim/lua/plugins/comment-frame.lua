89107c0966dce459f81855d7c0e26de5801b6882
Mahan <mahan.rahmati@protonmail.com>
2024-03-16 13:04:04 -0400

    Update comment-frame.lua

--- config/nvim/lua/plugins/comment-frame.lua
+++ config/nvim/lua/plugins/comment-frame.lua
@@ -1,13 +1,24 @@
 return {
   {
     "s1n7ax/nvim-comment-frame",
-    event = "VeryLazy",
+    event = { "BufReadPost", "BufNewFile" },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
     },
     opts = {
-      keymap = "<leader>Cs",
-      multiline_keymap = "<leader>Cm",
+      disable_default_keymap = true,
+    },
+    keys = {
+      {
+        "<leader>Cs",
+        "<cmd>lua require('nvim-comment-frame').add_comment()<CR>",
+        desc = "Comment Singleline",
+      },
+      {
+        "<leader>Cm",
+        "<cmd>lua require('nvim-comment-frame').add_multiline_comment()<CR>",
+        desc = "Comment Multiline",
+      },
     },
   },
 }
