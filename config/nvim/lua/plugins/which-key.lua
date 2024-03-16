4e0d4f736044cdcd82fd36a59977fd9771c9d817
Mahan <mahan.rahmati@protonmail.com>
2024-03-16 13:04:09 -0400

    Update which-key.lua

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -17,8 +17,6 @@ return {
       -- Document existing key chains
       require("which-key").register {
         ["<leader>C"] = { name = "Comment Frame", _ = "which_key_ignore" },
-        ["<leader>Cs"] = { name = "Comment Singleline", _ = "which_key_ignore" },
-        ["<leader>Cm"] = { name = "Comment Multiline", _ = "which_key_ignore" },
         ["<leader>f"] = { name = "Finder", _ = "which_key_ignore" },
         ["<leader>g"] = { name = "Git", _ = "which_key_ignore" },
         ["<leader>l"] = { name = "LSP", _ = "which_key_ignore" },
