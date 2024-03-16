ccd07cfffa877175c9461070d85fe9e03102a094
Mahan <mahan.rahmati@protonmail.com>
2024-03-16 17:05:22 -0400

    Update lspconfig.lua

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -46,6 +46,16 @@ return {
 
       vim.diagnostic.config(config)
 
+      vim.lsp.handlers["textDocument/hover"] =
+        vim.lsp.with(vim.lsp.handlers.hover, {
+          border = "rounded",
+        })
+
+      vim.lsp.handlers["textDocument/signatureHelp"] =
+        vim.lsp.with(vim.lsp.handlers.signature_help, {
+          border = "rounded",
+        })
+
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
         callback = function(event)
