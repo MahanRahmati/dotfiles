99ef9f7c692a17622d1a9364566e220ee73de129
Mahan <mahan.rahmati@protonmail.com>
2024-03-23 14:31:46 -0400

    [Neovim] Update conform

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -16,7 +16,16 @@ return {
         html = { { "prettier" } },
         ["*"] = { "squeeze_blanks" },
       },
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      format_on_save = function(bufnr)
+        local slow_filetypes = { dart = true }
+        local ft = vim.bo[bufnr].filetype
+        local slow = slow_filetypes[ft]
+        if slow then
+          return { timeout_ms = 2000, lsp_fallback = true }
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
+      format_after_save = { lsp_fallback = true },
       formatters = {
         prettier = {
           prepend_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
