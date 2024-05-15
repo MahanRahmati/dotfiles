ac428360e8804fdb0b1947fda7cbc610e8d0e1d7
Mahan <mahan.rahmati@protonmail.com>
2024-05-14 20:57:01 -0400

    [Neovim] Update sg

--- config/nvim/lua/plugins/sg.lua
+++ config/nvim/lua/plugins/sg.lua
@@ -1,4 +1,3 @@
--- TODO: Add Cody keys
 return {
   {
     "sourcegraph/sg.nvim",
@@ -9,42 +8,8 @@ return {
     opts = {
       enable_cody = true,
     },
-    -- keys = {
-    --   { "<leader>cc", "<cmd>CodyToggle<CR>", desc = "Cody Toggle" },
-    --   {
-    --     "<leader>ch",
-    --     "<cmd>lua require('sg.cody.commands').focus_history()<CR>",
-    --     desc = "Cody Focus History",
-    --   },
-    --   {
-    --     "<leader>cp",
-    --     "<cmd>lua require('sg.cody.commands').focus_prompt()<CR>",
-    --     desc = "Cody Focus Prompt",
-    --   },
-    --   {
-    --     "<leader>cd",
-    --     "<cmd>CodyAsk Generate code documentation<CR>",
-    --     mode = "v",
-    --     desc = "Cody Documentation",
-    --   },
-    --   {
-    --     "<leader>ce",
-    --     "<cmd>CodyAsk Explain code<CR>",
-    --     mode = "v",
-    --     desc = "Cody Explain",
-    --   },
-    --   {
-    --     "<leader>cu",
-    --     "<cmd>CodyAsk Generate unit tests<CR>",
-    --     mode = "v",
-    --     desc = "Cody Unit Test",
-    --   },
-    --   {
-    --     "<leader>cs",
-    --     "<cmd>CodyAsk Find code smells<CR>",
-    --     mode = "v",
-    --     desc = "Cody Smell",
-    --   },
-    -- },
+    keys = {
+      { "<leader>C", "<cmd>CodyToggle<CR>", desc = "Cody Toggle" },
+    },
   },
 }
