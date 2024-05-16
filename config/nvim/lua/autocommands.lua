81bf64987e98310683fde35c31569db8f605543d
Mahan <mahan.rahmati@protonmail.com>
2024-05-16 13:39:21 -0400

    [Neovim] Remove comments

--- config/nvim/lua/autocommands.lua
+++ config/nvim/lua/autocommands.lua
@@ -1,7 +1,3 @@
-----------------------------------------------------------------------
---                        Automatic Commands                        --
-----------------------------------------------------------------------
-
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
