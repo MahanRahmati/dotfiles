6593b5a3f96d12b7213180bb963cba7f10c30707
Mahan <mahan.rahmati@protonmail.com>
2024-06-02 10:48:20 -0400

    [Neovim] Update lazydev

--- config/nvim/lua/plugins/lazydev.lua
+++ config/nvim/lua/plugins/lazydev.lua
@@ -2,6 +2,14 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua",
-    opts = {},
+    opts = {
+      library = {
+        "luvit-meta/library",
+      },
+    },
+  },
+  {
+    "Bilal2453/luvit-meta",
+    lazy = true,
   },
 }
