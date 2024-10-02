595a580e91ce9e9c6799ad7dbd8eecb4ea7894d1
Mahan <mahan.rahmati@protonmail.com>
2024-10-02 15:13:38 -0400

    [Fish] Add jdk to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,6 +18,7 @@ fish_config theme choose "Catppuccin Mocha"
 fish_add_path .local/bin
 fish_add_path /usr/local/bin/
 fish_add_path .local/share/go/bin/
+fish_add_path /opt/homebrew/opt/openjdk@17/bin
 
 # ----------------------------------------------------------------------
 # --                               XDG                                --
