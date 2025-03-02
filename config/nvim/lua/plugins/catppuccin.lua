234d3da5dfbc09578827e4b237bf575ef52caa56
Mahan <mahan.rahmati@protonmail.com>
2025-03-02 16:03:06 -0500

    [Neovim] Update catppuccin

--- config/nvim/lua/plugins/catppuccin.lua
+++ config/nvim/lua/plugins/catppuccin.lua
@@ -2,89 +2,49 @@ return {
   {
     "catppuccin/nvim",
     priority = 1000,
-    opts = {
-      flavour = "mocha",
-      -- transparent_background = false,
-      no_underline = true,
-      color_overrides = {
-        mocha = {
-          -- Winbar
-          -- rosewater = "#F5E0DC",
-          -- Target word
-          -- flamingo = "#F2CDCD",
-          -- Just pink
-          -- pink = "#F5C2E7",
-          -- Tag
-          -- mauve = "#CBA6F7",
-          -- Error
-          -- red = "#F38BA8",
-          -- Lighter red
-          -- maroon = "#EBA0AC",
-          -- Number
-          -- peach = "#FAB387",
-          -- Warning
-          -- yellow = "#F9E2AF",
-          -- Diff add
-          -- green = "#A6E3A1",
-          -- Hint
-          -- teal = "#94E2D5",
-          -- Operator
-          -- sky = "#89DCEB",
-          -- Constructor
-          -- sapphire = "#74C7EC",
-          -- Diff changed
-          -- blue = "#89B4FA",
-          -- CursorLine Nr
-          -- lavender = "#B4BEFE",
-          -- Default fg
-          -- text = "#CDD6F4",
-          -- Indicator
-          -- subtext1 = "#BAC2DE",
-          -- Float title
-          -- subtext0 = "#A6ADC8",
-          -- Popup fg
-          -- overlay2 = "#9399B2",
-          -- Conceal color
-          -- overlay1 = "#7F849C",
-          -- Fold color
-          -- overlay0 = "#6C7086",
-          -- Default comment
-          -- surface2 = "#585B70",
-          -- Darker comment
-          -- surface1 = "#45475A",
-          -- Darkest comment
-          -- surface0 = "#313244",
-          base = "#0E1116", -- Default bg
-          mantle = "#0E1116", -- Darker bg
-          crust = "#585B70", -- Darkest bg
+    opts = function()
+      local colors = require "colors"
+      return {
+        flavour = "mocha",
+        transparent_background = true,
+        no_underline = true,
+        color_overrides = {
+          mocha = {
+            base = colors.background, -- Default bg
+            mantle = colors.background, -- Darker bg
+            crust = "#585B70", -- Darkest bg
+          },
         },
-      },
-      custom_highlights = function(colors)
-        return {
-          Pmenu = { bg = colors.base },
-          WhichKey = { fg = colors.blue },
-          WhichKeyGroup = { fg = colors.green },
-          WhichKeyDesc = { fg = colors.mauve },
-          TreesitterContext = { bg = "#242633" },
-          TreesitterContextLineNumber = { fg = colors.surface2 },
-          LineNr = { fg = colors.surface2 },
-          CursorLineNr = { fg = colors.yellow },
-          FloatBorder = { fg = colors.yellow },
-          DashboardHeader = { fg = colors.green },
-        }
-      end,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        illuminate = true,
-        mason = true,
-        notify = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
+        custom_highlights = function(c)
+          return {
+            Pmenu = { bg = c.base },
+            WhichKey = { fg = c.blue },
+            WhichKeyGroup = { fg = c.green },
+            WhichKeyDesc = { fg = c.mauve },
+            TreesitterContext = { bg = "#242633" },
+            TreesitterContextLineNumber = { fg = c.surface2 },
+            LineNr = { fg = c.surface2 },
+            CursorLineNr = { fg = c.yellow },
+            FloatBorder = { fg = c.yellow },
+            DashboardHeader = { fg = c.green },
+            NeoTreeFloatTitle = { fg = c.yellow },
+            NeoTreeDirectoryName = { fg = c.green, style = { "bold" } },
+            NeoTreeDirectoryIcon = { fg = c.green },
+          }
+        end,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          illuminate = true,
+          mason = true,
+          notify = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+          which_key = true,
+        },
+      }
+    end,
     config = function(_, opts)
       require("catppuccin").setup(opts)
       vim.cmd.colorscheme "catppuccin"
