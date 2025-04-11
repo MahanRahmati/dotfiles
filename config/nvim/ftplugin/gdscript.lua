daf409134da36f526963afb27405ac3f7598a45b
Mahan <mahan.rahmati@protonmail.com>
2025-04-11 09:43:10 -0400

    [Neovim] Update gdscript

---
+++ config/nvim/ftplugin/gdscript.lua
@@ -0,0 +1,30 @@
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 0
+
+local conform_ok, conform = pcall(require, "conform")
+if conform_ok then
+  conform.formatters_by_ft.gdscript = { "gdformat" }
+end
+
+local mason_ok, _ = pcall(require, "mason")
+if mason_ok then
+  local mason_tool_installer_ok, mason_tool_installer =
+    pcall(require, "mason-tool-installer")
+  if mason_tool_installer_ok then
+    mason_tool_installer.setup {
+      ensure_installed = {
+        "gdtoolkit",
+      },
+    }
+    vim.cmd "MasonToolsInstall"
+  end
+end
+
+local treesitter_ok, parsers = pcall(require, "nvim-treesitter.parsers")
+if treesitter_ok then
+  if not parsers.has_parser "gdscript" then
+    vim.cmd "TSInstall gdscript"
+  end
+end
