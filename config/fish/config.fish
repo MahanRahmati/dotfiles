5f08563e84e80d1f4fac75d2768c681efd4eb189
Mahan <mahan.rahmati@protonmail.com>
2026-01-13 23:02:57 -0500

    [Fish] Update config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -32,6 +32,7 @@ fish_add_path .local/bin
 fish_add_path /usr/local/bin/
 fish_add_path .local/share/go/bin/
 fish_add_path /opt/homebrew/opt/openjdk@17/bin
+fish_add_path /opt/homebrew/opt/rustup/bin
 
 # ----------------------------------------------------------------------
 # --                               XDG                                --
