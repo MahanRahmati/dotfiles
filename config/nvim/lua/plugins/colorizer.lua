a38e262a52835b6c6f96280db2201c6324ec5828
Mahan <mahan.rahmati@protonmail.com>
2026-03-04 20:41:46 -0500

    [Neovim] Update colorizer

--- config/nvim/lua/plugins/colorizer.lua
+++ config/nvim/lua/plugins/colorizer.lua
@@ -1,19 +1,13 @@
 return {
   {
     "catgoose/nvim-colorizer.lua",
-    event = { "BufReadPost", "BufNewFile" },
+    event = "BufReadPre",
     opts = {
-      user_default_options = {
-        names = true, -- "Name" codes like Blue or blue
-        RGB = true, -- #RGB hex codes
-        RGBA = true, -- #RGBA hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        AARRGGBB = true, -- 0xAARRGGBB hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      options = {
+        parsers = {
+          css = true,
+          css_fn = true,
+        },
       },
     },
   },
