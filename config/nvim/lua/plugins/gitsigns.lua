cb69e20d54cf53fda88b3c631e5d6fee1c4a61f7
Mahan <mahan.rahmati@protonmail.com>
2026-03-04 20:42:33 -0500

    [Neovim] Update gitsigns

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -13,6 +13,14 @@ return {
           changedelete = { text = icons.gitsigns.changedelete },
           untracked = { text = icons.gitsigns.untracked },
         },
+        signs_staged = {
+          add = { text = icons.gitsigns.add },
+          change = { text = icons.gitsigns.change },
+          delete = { text = icons.gitsigns.delete },
+          topdelete = { text = icons.gitsigns.topdelete },
+          changedelete = { text = icons.gitsigns.changedelete },
+          untracked = { text = icons.gitsigns.untracked },
+        },
         current_line_blame = true,
       }
     end,
